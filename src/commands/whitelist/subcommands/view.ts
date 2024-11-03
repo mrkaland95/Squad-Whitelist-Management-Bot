6f962aaf9e468142b9b355e03ec7b36a86f90f41
Flaxelaxen <mrkaland95@gmail.com>
2024-11-03 13:08:47 +0100

    Factored out whitelist embed to utility file.

--- src/commands/whitelist/subcommands/view.ts
+++ src/commands/whitelist/subcommands/view.ts
@@ -1,10 +1,8 @@
 import {
-    CommandInteraction,
-    EmbedBuilder,
-    User,
-    SlashCommandSubcommandBuilder,
+    SlashCommandSubcommandBuilder, ChatInputCommandInteraction,
 } from "discord.js";
-import {UsersDB} from "../../../db/schema";
+import { UsersDB } from "../../../db/schema";
+import { generateWhitelistEmbed } from "../utils/utils";
 
 
 export default {
@@ -12,7 +10,7 @@ export default {
         .setName('view')
         .setDescription(`Retrieves the current whitelisted IDs by the caller.`),
 
-    async execute(interaction: CommandInteraction) {
+    async execute(interaction: ChatInputCommandInteraction) {
         const user = await UsersDB.findOne({DiscordID: interaction.user.id})
         const steamIDs = user?.Whitelist64IDs
 
@@ -24,7 +22,7 @@ export default {
         }
 
         const embed = generateWhitelistEmbed(steamIDs, interaction.user)
-        const message = await interaction.followUp({
+        await interaction.followUp({
             ephemeral: true,
             embeds: [embed],
         })
@@ -32,41 +30,3 @@ export default {
 }
 
 
-/**
- * Generate the embed used to displaying a user's steamIDs.
- * @param steamIDs An array of objects containing a steamID and an optional user submitted name to describe whom it belongs to.
- * @param user The discord user object.
- */
-function generateWhitelistEmbed(steamIDs: { steamID: string; name?: string }[], user: User) {
-    const embed = new EmbedBuilder()
-        .setColor('Blurple')
-        .setTitle(`Whitelisted SteamIDs for user "${user.globalName}"`)
-        .setTimestamp(Date.now())
-
-    if (!steamIDs || !steamIDs.length) {
-        embed.setDescription(`No steamIDs to display`)
-        return embed
-    }
-
-    let formattedSteamIDs = ""
-    let formattedNames = ""
-
-    for (let i = 0; i < steamIDs.length; i++) {
-        const steamID = steamIDs[i].steamID
-        const name = steamIDs[i].name
-
-        formattedSteamIDs += `${i + 1}.) ${steamID}`
-        if (name) {
-            formattedNames += `${name}`
-        }
-        formattedSteamIDs += `\n`
-        formattedNames += `\n`
-    }
-
-    embed.addFields([
-        { name: 'SteamID', value: formattedSteamIDs, inline: true},
-        { name: 'Name', value: formattedNames, inline: true}
-    ])
-
-    return embed
-}
---
+++ src/commands/whitelist/utils/utils.ts
@@ -0,0 +1,40 @@
+import {EmbedBuilder, User} from "discord.js";
+
+/**
+ * Generates an embed used for displaying a Discord user's whitelisted steamIDs.
+ * @param steamIDs An array of objects containing a steamID and an optional user submitted name to describe whom it belongs to.
+ * @param user The discord user object.
+ */
+export function generateWhitelistEmbed(steamIDs: { steamID: string; name?: string }[], user: User) {
+    const embed = new EmbedBuilder()
+        .setColor('Blurple')
+        .setTitle(`Whitelisted SteamIDs for user "${user.globalName}"`)
+        .setTimestamp(Date.now())
+
+    if (!steamIDs || !steamIDs.length) {
+        embed.setDescription(`No steamIDs to display`)
+        return embed
+    }
+
+    let formattedSteamIDs = ""
+    let formattedNames = ""
+
+    for (let i = 0; i < steamIDs.length; i++) {
+        const steamID = steamIDs[i].steamID
+        const name = steamIDs[i].name
+
+        formattedSteamIDs += `${i + 1}.) ${steamID}`
+        if (name) {
+            formattedNames += `${name}`
+        }
+        formattedSteamIDs += `\n`
+        formattedNames += `\n`
+    }
+
+    embed.addFields([
+        { name: 'SteamID', value: formattedSteamIDs, inline: true},
+        { name: 'Name', value: formattedNames, inline: true}
+    ])
+
+    return embed
+}
